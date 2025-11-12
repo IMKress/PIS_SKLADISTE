c7bfd2ee5997d5e34bef4287c0a078349acf9218
Ivor M. K. <ivor4477@gmail.com>
2025-11-12 17:10:35 +0100

    merge

--- SKLADISTE - Copy - Copy/Skladiste.Model/obj/Debug/netcoreapp3.1/Skladiste.Model.AssemblyInfo.cs
+++ SKLADISTE - Copy - Copy/Skladiste.Model/obj/Debug/netcoreapp3.1/Skladiste.Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skladiste.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a70798f882a761ff2cc502b1e581867185faa72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c35a50920e43f259a0e5f6405a2e525c904693")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skladiste.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skladiste.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
